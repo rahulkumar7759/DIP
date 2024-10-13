daa17be3448bf461a83dbd2ec7953ffebca1c737
rahulumar7759 <rkj020@nist.edu>
2024-10-13 15:33:17 +0530

    edge

---
+++ Day-01/Edge-detection.py
@@ -0,0 +1,37 @@
+# Import the necessary Libraries
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+# Read image from disk.
+img = cv2.imread('kris.jpg')
+# Convert BGR image to RGB
+image_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+# Apply Canny edge detection
+edges = cv2.Canny(image= image_rgb, threshold1=100, threshold2=700)
+
+# Create subplots
+fig, axs = plt.subplots(1, 2, figsize=(7, 4))
+
+# Plot the original image
+axs[0].imshow(image_rgb)
+axs[0].set_title('Original Image')
+
+# Plot the blurred image
+axs[1].imshow(edges)
+axs[1].set_title('Image edges')
+
+# Remove ticks from the subplots
+for ax in axs:
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+# Display the subplots
+cv2.tight_layout()
+cv2.imshow()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
